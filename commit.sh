63b24c2a175cb75a84299674d9d1005306340255
Olampit <olampit@gmail.com>
2025-05-23 16:27:41 +0200

    routine commit 1

--- commit.sh
+++ commit.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# File to store the commit counter
+COUNTER_FILE=".commit_counter"
+
+# Initialize counter file if it doesn't exist
+if [ ! -f "$COUNTER_FILE" ]; then
+  echo 1 > "$COUNTER_FILE"
+fi
+
+# Read the current commit number
+count=$(cat "$COUNTER_FILE")
+
+# Git commands
 git add .
-git commit --author="Olampit <olampit@gmail.com>" -m "routine commit"
+git commit --author="Olampit <olampit@gmail.com>" -m "routine commit $count"
 git push -u origin main
+
+# Increment and save back
+echo $((count + 1)) > "$COUNTER_FILE"
