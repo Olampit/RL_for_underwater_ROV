4b2aa6fe5523a5e4e2af3c90b82cd67973017855
basile.dumas <basile.dumas@gmail.com>
2025-06-24 18:49:07 +0200

    good commit for deterministic on new branch

--- build/Stonefish/CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer.cpp.o: \
- /home/mig/Documents/RL/stonefish/stonefish/3rdparty/LinearMath/btSerializer.cpp \
- /usr/include/stdc-predef.h
--- build/Stonefish/CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer64.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Stonefish.dir/3rdparty/LinearMath/btSerializer64.cpp.o: \
- /home/mig/Documents/RL/stonefish/stonefish/3rdparty/LinearMath/btSerializer64.cpp \
- /usr/include/stdc-predef.h
